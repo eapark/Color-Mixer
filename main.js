989d23d1cd7e6827a02b6c07536c70706864712a
Emily Park <eapark@emilys-air.dhcp.nd.edu>
2017-12-09 00:09:56 -0500

    Chroma file added Added function getScore

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var gl;
 var canvas;
 var answerColor = [];
 var userColor   = [];
+var gameOver = false;
+var increment = 0.05;
 
 window.onload = function init()
 {
@@ -21,7 +23,14 @@ window.onload = function init()
     gl.enable(gl.DEPTH_TEST);
 
     // Set answerColor to be a random color
-    answerColor = [ Math.random(), Math.random(), Math.random ];
+    answerColor = [ Math.random(), Math.random(), Math.random() ];
+
+    // Alternative: set answerColor to be a multiple of 'increment'
+    // So that it's easier to get the right answer
+    for(var i=0; i<3; i++) {
+        answerColor[i] = increment * Math.floor( Math.random() * 1.0/0.05 )
+    }
+
 
     // HTML button event listeners
     document.getElementById( "addA" ).onclick = function() {
@@ -41,7 +50,7 @@ window.onload = function init()
 // Check user's answer with answerColor
 function checkAnswer() {
     var range = 0.05; //Acceptable error margin
-    if( Math.abs( answerColor[0] - userColor[0]) <= range &&
+    if( Math.abs( answerColor[0] - userColor[0] ) <= range &&
         Math.abs( answerColor[1] - userColor[1] ) <= range &&
         Math.abs( answerColor[2] - userColor[2] ) <= range )
     {
@@ -51,6 +60,17 @@ function checkAnswer() {
     {
         return false;
     }
+}
+
+// Return a score out of 100
+// Score is the average accuracy
+function getScore() {
+    var sum = 0;
+    for (var i=0; i<3; i++) {
+        sum += 1.0 - Math.abs((answerColor[i] - userColor[i]) / answerColor[i]);
+    }
+    gameOver = true;
+    return (sum / 3) * 100;
 }
 
 // User can start over
